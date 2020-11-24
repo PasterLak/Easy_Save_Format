d57d60b22c4ae9b9f2857b939ddfc15320f31f11
PasterLak <paster.lak@gmail.com>
2020-11-24 14:13:58 +0100

    Added script

---
+++ EsfFile.cs
@@ -0,0 +1,336 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public sealed class EsfFile
+{
+    private Dictionary<string, string> dictionary = new Dictionary<string, string>();
+
+    private List<string> addedKeys = new List<string>();
+
+    public bool SaveOriginalStructure = true;
+
+    public EsfFile()
+    {
+
+    }
+
+    public EsfFile(Dictionary<string, string> dictionary)
+    {
+        this.dictionary = dictionary;
+    }
+
+    public EsfFile(string path)
+    {
+        Read(path);
+
+    }
+
+    public void SetValue<T>(string key, T value) where T : IConvertible
+    {
+        if (dictionary.Keys.Count == 0) return;
+
+        if (dictionary.ContainsKey(key))
+        {
+            if(!value.GetType().IsArray)
+            dictionary[key] = value.ToString();
+            else
+            {
+
+                dictionary[key] = MassiveToText((IConvertible[])(object)value);
+            }
+        }
+        else
+        {
+            AddValue(key, value);
+        }
+
+    }
+
+    public void SetValue<T>(string key, T[] value) 
+    {
+        if (dictionary.Keys.Count == 0) return;
+
+        if (dictionary.ContainsKey(key))
+        {
+           
+            dictionary[key] = MassiveToText(value);
+            
+        }
+        else
+        {
+            AddValue(key, value);
+        }
+
+    }
+
+    private void AddValue<T>(string key, T value) where T : IConvertible
+    {
+
+        if (!dictionary.ContainsKey(key))
+        {
+          
+                dictionary.Add(key, value.ToString());
+                addedKeys.Add(key);
+        }
+        else
+        {
+            Debug.LogError(key + " already exist!");
+        }
+     
+    }
+
+    private void AddValue<T>(string key, T[] value)
+    {
+
+        if (!dictionary.ContainsKey(key))
+        {
+         
+               dictionary.Add(key, MassiveToText(value));
+               addedKeys.Add(key);
+            
+        }
+        else
+        {
+            Debug.LogError(key + " already exist!");
+        }
+
+    }
+
+    public void RemoveValue(string key)
+    {
+
+        if (dictionary.ContainsKey(key))
+        {
+            dictionary.Remove(key);
+           
+        }
+
+    }
+
+    public T GetValue<T>(string key) where T : IConvertible
+    {
+        if (dictionary.Keys.Count == 0 & !dictionary.ContainsKey(key))
+            return (T)(object)null;
+
+        return (T)Convert.ChangeType(dictionary[key], typeof(T));
+
+    }
+
+    public T[] GetArray<T>(string key) where T : IConvertible
+    {
+        if (dictionary.Keys.Count == 0 && !dictionary.ContainsKey(key))
+            return (T[])(object)null;
+
+        return ReadMassive<T>(dictionary[key]);
+
+
+    }
+
+    public void GetValue<T>(string key, out T value)
+    {
+        value = (T)(object)null;
+
+        if (dictionary.Keys.Count == 0 && !dictionary.ContainsKey(key))
+            return;
+
+        value = (T)Convert.ChangeType(dictionary[key], typeof(T));
+
+    }
+
+    public void GetValue<T>(string key, out T[] array) where T : IConvertible
+    {
+        array = null;
+
+        if (dictionary.Keys.Count == 0) return;
+        
+        if(dictionary.ContainsKey(key))
+            array = ReadMassive<T>(dictionary[key]);
+
+    }
+
+    public void Write(string path)
+    {
+
+
+        if (SaveOriginalStructure == false)
+        {
+            List<string> line = new List<string>();
+
+            foreach (string key in dictionary.Keys)
+            {
+                line.Add(key + " = " + dictionary[key]);
+            }
+
+            File.WriteAllLines(path, line.ToArray());
+
+            addedKeys.Clear();
+
+            return;
+        }
+
+        
+        string[] lines = new string[0];
+        List<string> linesUpdated = new List<string>();
+
+        List<string> newLines = new List<string>();
+        List<string> keys = new List<string>();
+
+        if (File.Exists(path))
+        {
+            lines = File.ReadAllLines(path);
+           
+        }
+ 
+
+        for (int i = 0; i < lines.Length; i++)
+        {
+            if (TryReadLine(lines[i], out string key, out string _))
+            {
+                keys.Add(key);
+
+                if (dictionary.ContainsKey(key) == true)
+                {
+
+                    string value = dictionary[key];
+
+                    //linesUpdated.Add(key + " = " + value);
+                    lines[i] = key + " = " + value;
+                }
+
+            }
+        }
+
+        if (File.Exists(path))
+            File.WriteAllLines(path, lines);
+        else File.Create(path).Close();
+
+        if (addedKeys.Count > 0)
+        {
+
+            for (int i = 0; i < addedKeys.Count; i++)
+            {
+                if (!keys.Contains(addedKeys[i]))
+                {
+                  
+                    newLines.Add(addedKeys[i] + " = " + dictionary[addedKeys[i]] );
+
+                }
+            }
+
+            addedKeys.Clear();
+
+        }
+
+        if (newLines.Count > 0)
+        File.AppendAllLines(path, newLines.ToArray());
+    }
+
+    public void Delete(string path)
+    {
+        if(File.Exists(path))
+        {
+            File.Delete(path);
+        }
+    }
+
+    private void Read(string path)
+    {
+        if (File.Exists(path))
+        {
+            
+            string[] lines = File.ReadAllLines(path);
+
+            foreach (string line in lines)
+            {
+                if(TryReadLine(line, out string key, out string value))
+                {
+                    
+                    if(dictionary.ContainsKey(key) == false)
+                    {
+                        
+                        dictionary.Add(key, value);
+                    }
+                    
+
+                }
+            }
+        }
+        
+    }
+
+    private string MassiveToText<T>(T[] array)
+    {
+        string value = "[";
+
+        if (array == null) return "ERROR";
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            value += array[i].ToString();
+            if (i < array.Length - 1) value += ",";
+        }
+
+        value += "]";
+
+        return value;
+    }
+
+    private bool TryReadLine(string line, out string key, out string value)
+    {
+        key = "";
+        value = "";
+
+        if (String.IsNullOrWhiteSpace(line)) return false;
+        if (line.Contains("=") == false) return false;
+        if (line[0] == '/' & line[1] == '/') return false;
+        if (line[0] == '#') return false;
+
+        string[] parts = line.Split('=');
+
+        if (parts.Length == 2)
+        {
+            key = parts[0].Trim(' ');
+            key = key.Replace(" ", string.Empty);
+
+            parts[1] = parts[1].Trim(' ');
+
+            if(parts[1][0] == '"' & parts[1][parts[1].Length - 1] == '"')
+             parts[1] = parts[1].Substring(1, parts[1].Length - 2);
+
+            value = parts[1];
+            return true;
+
+        }
+
+        return false;
+    }
+
+    private T[] ReadMassive<T>(string value) where T : IConvertible
+    {
+        string[] data;
+        T[] num;
+
+        if (typeof(T) != typeof(string))
+        value = value.Replace(" ", string.Empty);
+        value = value.Replace("[", string.Empty);
+        value = value.Replace(",]", string.Empty);
+        value = value.Replace("]", string.Empty);
+
+        data = value.Split(',');
+        num = new T[data.Length];
+
+        for (int i = 0; i < data.Length; i++)
+        {
+            if (data[i] != string.Empty)
+                num[i] = (T)Convert.ChangeType(data[i], typeof(T));
+            else
+                Debug.LogError("Empty string!");
+        }
+       
+        return num;
+        
+    }
+   
+}
